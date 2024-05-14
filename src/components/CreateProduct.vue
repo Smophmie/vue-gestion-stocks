04837e3f1f04a0e0260f5e1d040deda1cf129d56
Sophie Théréau <sophiethereau@hotmail.fr>
2024-05-14 10:33:35 +0200

    Create product fonctionnel

---
+++ src/components/CreateProduct.vue
@@ -0,0 +1,85 @@
+<script setup>
+
+import { ref, onMounted, reactive } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+
+const availableCategories = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/v1/categories')
+    availableCategories.value = response.data
+  } catch (error) {
+    errorMessage.value = error.message
+  }
+})
+
+let form = reactive({
+  name: "",
+  price: 0,
+  stock: 0,
+  categories: []
+})
+
+const createProduct = async () => {
+  try {
+    await axios.post('http://127.0.0.1:8000/api/v1/products', form)
+    await router.push({name:"showproducts"})
+  } catch (error) {
+    errorMessage.value = error.message
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="green">Créer un produit</h1>
+  </div>
+
+  <form @submit.prevent="createProduct()">
+    <div class="form_part">
+      <label for="name">Nom du produit</label>
+      <input type="text" id="name" v-model="form.name" placeholder="Mon produit">
+    </div>
+    <div class="form_part">
+      <label for="price">Prix</label>
+      <input type="text" id="price" v-model="form.price" placeholder="5.00">
+    </div>
+    <div class="form_part">
+      <label for="stock">Stock actuel</label>
+      <input type="text" id="stock" v-model="form.stock" placeholder="100">
+    </div>
+    <div class="form_part">
+      <label>Catégorie(s) associée(s)</label>
+      <div v-for="category in availableCategories" :key="category.id" class="checkboxContainer">
+        <input type="checkbox"  :value="category.id" v-model="form.categories">
+        <label>{{ category.title }}</label>
+      </div>
+    </div>
+    <div>
+      <button type="submit" class="confirm_button">Créer</button>
+    </div>
+  </form>
+
+</template>
+
+<style scoped>
+
+.form_part {
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  width: 300px;
+}
+
+.checkboxContainer {
+  display: flex;
+  font-display: row;
+  gap: 10px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 import Welcome from '../components/Welcome.vue'
 import ShowProducts from '../components/ShowProducts.vue'
 import ShowProduct from '../components/ShowProduct.vue'
+import CreateProduct from '../components/CreateProduct.vue'
 import ShowCategories from '../components/ShowCategories.vue'
 import ShowCategory from '../components/ShowCategory.vue'
 import CreateCategory from '../components/CreateCategory.vue'
@@ -15,6 +16,7 @@ const router = createRouter({
     { path: '/', name:"welcome", component: Welcome },
     { path: '/products', name:"showproducts", component: ShowProducts },
     { path: '/products/:id', name:"showproduct", component: ShowProduct },
+    { path: '/products/create', name:"createproduct", component: CreateProduct },
     { path: '/categories', name:"showcategories", component: ShowCategories },
     { path: '/categories/:id', name:"showcategory", component: ShowCategory },
     { path: '/categories/create', name:"createcategory", component: CreateCategory }
